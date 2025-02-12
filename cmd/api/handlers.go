ab71de1436bc58c5dc22a58dda988e06f6ef5266
Diego-All <dposadallano@hotmail.com>
2025-02-12 00:11:57 -0500

    asimetric token update

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -216,6 +216,79 @@ func (app *application) LoginJWT(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (app *application) LoginAsimJWT(w http.ResponseWriter, r *http.Request) {
+
+	type credentials struct {
+		UserName string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var creds credentials
+	var payload jsonResponse
+
+	err := app.readJSON(w, r, &creds)
+	if err != nil {
+		app.errorLog.Println(err)
+		payload.Error = true
+		payload.Message = "invalid json supplied, or json missing entirely"
+		_ = app.writeJSON(w, http.StatusBadRequest, payload)
+	}
+
+	// TODO authenticate
+	app.infoLog.Println(creds.UserName, creds.Password)
+
+	fmt.Println("LOGINJWT")
+
+	// look up the user by email
+	// Security Advice: Change output messages to avoid enumerate users attack
+	user, err := app.models.User.GetByEmail(creds.UserName)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid username/password"))
+		return
+	}
+
+	// fmt.Println("USER:", user)
+
+	// validate the user's password
+	validPassword, err := user.PasswordMatches(creds.Password)
+	if err != nil || !validPassword {
+		app.errorJSON(w, errors.New("invalid username/password"))
+		return
+	}
+
+	fmt.Println("VALID PASSWORD", validPassword)
+
+	// we have a valid user, so generate a token
+	token, err := app.models.JWTToken.GenerateJWTToken(creds.UserName, user.ID) // userID ?? DURATION FROM MAIN
+	//token, err := app.models.Token.GenerateToken(user.ID, 24*time.Hour)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	fmt.Println("TOKEN", token)
+
+	// save it to the database
+	err = app.models.JWTToken.InsertJWT(*token, *user)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	// send back a response
+	payload = jsonResponse{
+		Error:   false,
+		Message: "logged in",
+		Data:    envelope{"token": token, "user": user},
+	}
+
+	err = app.writeJSON(w, http.StatusOK, payload)
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+
+}
+
 func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
 	var requestPayload struct {
 		Token string `json:"token"`
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,6 +19,7 @@ type config struct {
 	jwt_secret     string
 	hash_cost      int // bcrypt
 	token_duration time.Duration
+	jwt_PrivateKey string
 }
 
 type application struct {
@@ -42,6 +43,7 @@ func main() {
 
 	cfg.certPath = "/home/diegoall/MAESTRIA_ING/domain-model/products-API/cmd/api/server.pem"
 	cfg.keyPath = "/home/diegoall/MAESTRIA_ING/domain-model/products-API/cmd/api/server.key"
+	cfg.jwt_PrivateKey = "/home/diegoall/MAESTRIA_ING/OAuth/jwt-rbac-products-API/cmd/api/ec_private.pem"
 	cfg.jwt_secret = "secret"
 	cfg.token_duration = 24 * time.Hour
 	cfg.hash_cost = 8
